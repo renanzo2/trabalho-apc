3a8f51eb577de712cbfc6304ba7ccc36cd9cad7b
vihtyn <167125772+vihtyn@users.noreply.github.com>
2024-09-29 18:49:56 -0300

    Add files via upload

---
+++ função_val.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    
+    int x, b;
+    double funcao;
+    double a0 = 186.752, a1 = -148.235, a2 = 34.5049, a3= -3.5091, a4 = 0.183166, a5 = -0.00513554, a6 = 0.0000735464, a7 = -4.22038 * pow(10, -7);
+
+    funcao = a0 + (a1 + b) * x + a2 * pow(x, 2) + a3 * pow(x, 3) + a4 * pow(x, 4) + a5 * pow(x, 5) + a6 * pow(x, 6) + a7 * pow(x, 7);
+}
